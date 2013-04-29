6da2be39a95cf4676193bfac529e6cda2a9d88aa
newgene <anewgene@yahoo.com>
2013-04-29 11:19:30 -0700

    Simplified API end-points.

--- src/api_v2/handlers.py
+++ src/api_v2/handlers.py
@@ -18,45 +18,13 @@ class GeneHandler(BaseHandler):
         '''
         if geneid:
             kwargs = self.get_query_params()
+            kwargs.setdefault('scopes', 'entrezgene,ensemblgene,retired')
+            kwargs.setdefault('species', 'all')
             gene = self.esq.get_gene2(geneid, **kwargs)
-            self.return_json(gene)
-        else:
-            raise tornado.web.HTTPError(404)
-
-    def post(self):
-        '''
-           post to /gene
-
-           parameters:
-                ids
-                fields
-                scopes
-                species
-
-           with parameters of
-            {'ids': '1017,1018',
-             'fields': 'symbol,name'}
-
-            {'ids': '1017',
-             'fields': 'symbol,name,reporter.HG-U133_Plus_2'}
-
-            {'ids': 'NM_006445',
-             'scopes': 'refseq',
-             'fields': 'symbol,name,reporter.HG-U133_Plus_2'}
-
-            {'ids': 'CDK2',
-             'scopes': 'symbol',
-             'species': '9606',
-             'fields': 'symbol,name,reporter.HG-U133_Plus_2'}
-
-
-        '''
-        kwargs = self.get_query_params()
-        geneids = kwargs.pop('ids', None)
-        if geneids:
-            geneids = [_id.strip() for _id in geneids.split(',')]
-            res = self.esq.mget_gene2(geneids, **kwargs)
-            self.return_json(res)
+            if gene:
+                self.return_json(gene)
+            else:
+                raise tornado.web.HTTPError(404)
         else:
             raise tornado.web.HTTPError(404)
 
@@ -72,14 +40,13 @@ class QueryHandler(BaseHandler):
             from
             size
             sort
+            species
 
             explain
-            sample
         '''
         kwargs = self.get_query_params()
         q = kwargs.pop('q', None)
         if q:
-            #fields = kwargs.get('fields', None)
             explain = self.get_argument('explain', None)
             if explain and explain.lower()=='true':
                 kwargs['explain'] = True
@@ -87,12 +54,7 @@ class QueryHandler(BaseHandler):
                 value = kwargs.get(arg, None)
                 if value:
                     kwargs[arg] = int(value)
-            sample = kwargs.get('sample', None) == 'true'
-
-            if sample:
-                res = self.esq.query_sample(q, **kwargs)
-            else:
-                res = self.esq.query(q, **kwargs)
+            res = self.esq.query(q, **kwargs)
         else:
             res = {'success': False, 'error': "Missing required parameters."}
 
@@ -122,41 +84,9 @@ class QueryHandler(BaseHandler):
         self.return_json(res)
 
 
-
-class IntervalQueryHandler(BaseHandler):
-    def get(self):
-        #/interval?interval=chr12:56350553-56367568&taxid=9606
-        interval = self.get_argument('interval', None)
-        taxid = self.get_argument('taxid', None)
-        kwargs = {}
-        if interval and taxid:
-            kwargs['taxid'] = int(taxid)
-            pattern = r'chr(?P<chr>\w+):(?P<gstart>[0-9,]+)-(?P<gend>[0-9,]+)'
-            mat = re.search(pattern, interval)
-            if mat:
-                kwargs.update(mat.groupdict())
-            fields = self.get_argument('fields', None)
-            if fields:
-                fields = fields.split(',')
-                kwargs['fields'] = fields
-            explain = self.get_argument('explain', None)
-            if explain and explain.lower()=='true':
-                kwargs['explain'] = True
-            for arg in ['from', 'size', 'mode']:
-                value = self.get_argument(arg, None)
-                if value:
-                    kwargs[arg] = int(value)
-            #sample = self.get_argument('sample', None) == 'true'
-            esq = ESQuery()
-            res = esq.query_interval(**kwargs)
-            self.return_json(res)
-
-
 APP_LIST = [
 
         (r"/gene/([\w\-\.]+)/?", GeneHandler),   #for get request
-        (r"/gene/?", GeneHandler),               #for post request
         (r"/query/?", QueryHandler),
-        (r"/interval/?", IntervalQueryHandler),
 
 ]
