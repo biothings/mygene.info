0a292cdd7cd81190fb06e36f6f7b2bc225a02499
newgene <anewgene@yahoo.com>
2013-06-19 10:24:09 -0700

    added tracking for visitor.locale.

--- src/utils/ga.py
+++ src/utils/ga.py
@@ -9,12 +9,16 @@ class GAMixIn:
                         _req.headers.get("X-Forwarded-For",
                         _req.remote_ip))
             user_agent = _req.headers.get("User-Agent", None)
-            tracker = Tracker(config.GA_ACCOUNT, 'mygene.info')
             visitor = Visitor()
             visitor.ip_address = remote_ip
             visitor.user_agent = user_agent
+            #get visitor.locale
+            visitor.extract_from_server_meta(
+                {"HTTP_ACCEPT_LANGUAGE": _req.headers.get("Accept-Language", None)}
+            )
             session = Session()
             page = Page(_req.path)
+            tracker = Tracker(config.GA_ACCOUNT, 'mygene.info')
             tracker.track_pageview(page, session, visitor)
             if event:
                 evt = Event(**event)
