2bac34eb7951acce6f230e1bdcfaf077ea422ec8
newgene <anewgene@yahoo.com>
2013-07-03 14:54:14 -0700

    small re-org of conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,8 +11,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sphinx_bootstrap_theme
-
 import sys, os
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -97,17 +95,12 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'default'
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
-html_theme_options = {
-'bootswatch_theme': "simplex",
-'navbar_site_name': "Index",
-}
+
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
@@ -253,3 +246,12 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+
+import sphinx_bootstrap_theme
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme_options = {
+'bootswatch_theme': "simplex",
+'navbar_site_name': "Index",
+}
