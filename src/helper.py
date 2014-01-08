b951caac3678df1bb7461c3011e47e942b19faf3
newgene <anewgene@yahoo.com>
2014-01-07 18:25:19 -0800

    added dotfield as boolean parameter.

--- src/helper.py
+++ src/helper.py
@@ -16,7 +16,7 @@ class BaseHandler(tornado.web.RequestHandler, GAMixIn):
     jsonp_parameter = 'callback'
     cache_max_age = 604800  # 7days
     disable_caching = False
-    boolean_parameters = set(['raw', 'rawquery', 'entrezonly', 'ensemblonly'])
+    boolean_parameters = set(['raw', 'rawquery', 'entrezonly', 'ensemblonly', 'dotfield'])
 
     def _check_fields_param(self, kwargs):
         '''support "filter" as an alias of "fields" parameter for back-compatability.'''
