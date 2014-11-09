1da697cf3fd3140a83b04ddeeb1db06b1583afa7
newgene <anewgene@yahoo.com>
2014-11-09 10:28:35 -0800

    allow include_tax_tree to be a boolean query parameter.

--- src/helper.py
+++ src/helper.py
@@ -29,7 +29,7 @@ class BaseHandler(tornado.web.RequestHandler, GAMixIn):
     jsonp_parameter = 'callback'
     cache_max_age = 604800  # 7days
     disable_caching = False
-    boolean_parameters = set(['raw', 'rawquery', 'entrezonly', 'ensemblonly', 'dotfield'])
+    boolean_parameters = set(['raw', 'rawquery', 'entrezonly', 'ensemblonly', 'dotfield', 'include_tax_tree'])
 
     def _check_fields_param(self, kwargs):
         '''support "filter" as an alias of "fields" parameter for back-compatability.'''
