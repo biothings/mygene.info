2d359258faf2f795870aab6e2a987259e725b3fc
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2017-01-09 12:53:35 -0800

    Adding handlers for the demo and taxon endpoints

--- src/www/api/handlers.py
+++ src/www/api/handlers.py
@@ -3,6 +3,7 @@ from biothings.www.api.es.handlers import BiothingHandler
 from biothings.www.api.es.handlers import MetadataHandler
 from biothings.www.api.es.handlers import QueryHandler
 from biothings.www.api.es.handlers import StatusHandler
+from tornado.web import RequestHandler
 
 class GeneHandler(BiothingHandler):
     ''' This class is for the /gene endpoint. '''
@@ -16,10 +17,16 @@ class StatusHandler(StatusHandler):
     ''' This class is for the /status endpoint. '''
     pass
 
-"""class FieldsHandler(FieldsHandler):
-    ''' This class is for the /metadata/fields endpoint. '''
-    pass"""
-
 class MetadataHandler(MetadataHandler):
     ''' This class is for the /metadata endpoint. '''
     pass
+
+class TaxonHandler(RequestHandler):
+    def get(self, taxid):
+        self.redirect("http://t.biothings.io/v1/taxon/%s?include_children=1" % taxid)
+
+class DemoHandler(RequestHandler):
+    def get(self):
+        with open('../docs/demo/index.html', 'r') as demo_file: 
+            self.write(demo_file.read())
+    
