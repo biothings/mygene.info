6ceae985fcce70877a96ca56279361ad18ebe493
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2017-07-25 17:22:45 -0700

    Species index switching.

--- src/web/api/handlers.py
+++ src/web/api/handlers.py
@@ -5,13 +5,22 @@ from biothings.web.api.es.handlers import QueryHandler
 from biothings.web.api.es.handlers import StatusHandler
 from tornado.web import RequestHandler
 
+def get_es_index(inst, options):
+    if 'all' in options.esqb_kwargs.species or len(set(options.esqb_kwargs.species)-
+        set([x['tax_id'] for x in inst.web_settings.TAXONOMY.values()])) > 0:
+        return inst.web_settings.ES_INDEX
+    else:
+        return inst.web_settings.ES_INDEX_TIER1
+
 class GeneHandler(BiothingHandler):
     ''' This class is for the /gene endpoint. '''
-    pass
+    def _get_es_index(self, options):
+        return get_es_index(self, options)
 
 class QueryHandler(QueryHandler):
     ''' This class is for the /query endpoint. '''
-    pass
+    def _get_es_index(self, options):
+        return get_es_index(self, options)
 
 class StatusHandler(StatusHandler):
     ''' This class is for the /status endpoint. '''
