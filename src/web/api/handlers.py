8ec066f293932bc0bdc25665d51447a06bec51b6
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2017-08-03 12:41:12 -0700

    Adding taxon redirect, subclassing some hooks to do species redirect

--- src/web/api/handlers.py
+++ src/web/api/handlers.py
@@ -4,6 +4,8 @@ from biothings.web.api.es.handlers import MetadataHandler
 from biothings.web.api.es.handlers import QueryHandler
 from biothings.web.api.es.handlers import StatusHandler
 from tornado.web import RequestHandler
+import requests
+#import logging
 
 def get_es_index(inst, options):
     if 'all' in options.esqb_kwargs.species or len(set(options.esqb_kwargs.species)-
@@ -19,6 +21,20 @@ class GeneHandler(BiothingHandler):
 
 class QueryHandler(QueryHandler):
     ''' This class is for the /query endpoint. '''
+    def _pre_query_builder_GET_hook(self, options):
+        if options.esqb_kwargs.include_tax_tree and 'all' not in options.esqb_kwargs.species:
+            headers = {'content-type': 'application/x-www-form-urlencoded',
+                        'user-agent': 'Python-requests_mygene.info/{} (gzip)'.format(requests.__version__)}
+
+            res = requests.post(self.web_settings.INCLUDE_TAX_TREE_REDIRECT_TEMPLATE.format(
+                ids=','.join(['{}'.format(sid) for sid in options.esqb_kwargs.species])), headers=headers)
+
+            if res.status_code == requests.codes.ok:
+                options['esqb_kwargs']['species'] = [str(x) for x in res.json()]
+                #logging.debug('tax_tree species: {}'.format(options.esqb_kwargs.species))
+
+        return options
+
     def _get_es_index(self, options):
         return get_es_index(self, options)
 
@@ -31,6 +47,9 @@ class MetadataHandler(MetadataHandler):
     pass
 
 class TaxonHandler(RequestHandler):
+    def initialize(self, web_settings):
+        pass
+
     def get(self, taxid):
         self.redirect("http://t.biothings.io/v1/taxon/%s?include_children=1" % taxid)
 
