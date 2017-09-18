1de15bf1a02a73776effd2a03c4f8bafce20a56c
Sebastien Lelong <slelong@scripps.edu>
2017-09-18 11:36:14 -0700

    uniprot pdb/pir as dependent from uniprot (dumper & uploaders)

--- src/hub/dataload/sources/uniprot/dump.py
+++ src/hub/dataload/sources/uniprot/dump.py
@@ -8,7 +8,8 @@ import biothings, config
 biothings.config_for_app(config)
 
 from config import DATA_ARCHIVE_ROOT, logger as logging
-from biothings.hub.dataload.dumper import FTPDumper
+from biothings.hub.dataload.dumper import FTPDumper, FilesystemDumper
+from biothings.utils.hub_db import get_src_dump
 
 
 class UniprotDumper(FTPDumper):
@@ -41,3 +42,35 @@ class UniprotDumper(FTPDumper):
             if force or not os.path.exists(local_file) or self.remote_is_better(fn,local_file) or self.new_release_available():
                 self.to_dump.append({"remote": fn, "local":local_file})
 
+
+class UniprotDependentDumper(FilesystemDumper):
+
+    UNIPROT_FILE = None # tbd
+
+    def create_todump_list(self, force=False):
+        uni_doc = get_src_dump().find_one({"_id":UniprotDumper.SRC_NAME}) or {}
+        if uni_doc:
+            remotefile = os.path.join(uni_doc["data_folder"],self.__class__.UNIPROT_FILE)
+            if not os.path.exists(remotefile):
+                self.logger.warning("File '%s' doesn't exist (yet?)" % self.__class__.UNIPROT_FILE)
+                return
+            self.release = uni_doc["release"]
+            localfile = os.path.join(self.current_data_folder,self.__class__.UNIPROT_FILE)
+            if force or not os.path.exists(localfile) or self.remote_is_better(remotefile,localfile):
+                self.to_dump.append({"remote":remotefile,"local":localfile})
+        else:
+            self.logger.error("Dependent uniprot datasource has not been loaded (not src_dump doc)")
+
+
+class UniprotPDBDumper(UniprotDependentDumper):
+    SRC_NAME = "uniprot_pdb"
+    SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
+    SCHEDULE = "0 11 * * *"
+    UNIPROT_FILE = "gene2pdb.pyobj"
+
+
+class UniprotPIRDumper(UniprotDependentDumper):
+    SRC_NAME = "uniprot_pir"
+    SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
+    SCHEDULE = "0 11 * * *"
+    UNIPROT_FILE = "gene2pir.pyobj"
--- src/hub/dataload/sources/uniprot/uniprot_upload.py
+++ src/hub/dataload/sources/uniprot/uniprot_upload.py
@@ -1,9 +1,13 @@
+import os
+
 from .parser import load_all, load_pir, load_pdb
 import biothings.hub.dataload.uploader as uploader
 import biothings.hub.dataload.storage as storage
 import biothings.utils.mongo as mongo
+import config
 
-
+from .pdb_upload import UniprotPDBUploader
+from .pir_upload import UniprotPIRUploader
 
 class UniprotUploader(uploader.MergerSourceUploader):
 
@@ -15,12 +19,21 @@ class UniprotUploader(uploader.MergerSourceUploader):
         return genedoc_d
 
     def post_update_data(self, steps, force, batch_size, job_manager, **kwargs):
-        pir_data = load_pir(self.data_folder)
-        pir_storage = storage.MergerStorage(mongo.get_src_db(),"uniprot_pir",self.logger)
-        pir_storage.process(pir_data,batch_size)
-        pdb_data = load_pdb(self.data_folder)
-        pdb_storage = storage.MergerStorage(mongo.get_src_db(),"uniprot_pdb",self.logger)
-        pdb_storage.process(pdb_data,batch_size)
+        # move produced files used for other dependent uploaders
+        klass = {"pir":UniprotPIRUploader,"pdb":UniprotPDBUploader}
+        release = os.path.split(self.data_folder)[-1]
+        for ext in ["pir","pdb"]:
+            destdir = os.path.join(config.DATA_ARCHIVE_ROOT,klass[ext].name,release)
+            destfn = "gene2%s.pyobj" % ext
+            try:
+                os.makedirs(destdir)
+            except FileExistsError:
+                # good to go
+                pass
+            self.logger.info("Dispatching file '%s' to %s upload" % (destfn,ext.upper()))
+            os.rename(os.path.join(self.data_folder,destfn),
+                    os.path.join(destdir,destfn))
+            uploader.set_pending_to_upload(klass[ext].name)
 
     @classmethod
     def get_mapping(klass):
