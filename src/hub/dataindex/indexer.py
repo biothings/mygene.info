672d55f2deab4c7c0e9b7249d22ccca58340a884
Sebastien Lelong <sebastien.lelong@gmail.com>
2017-11-27 06:48:48 -0800

    fix missing auto_expand_replicas for index creation + lsmerge cmd

--- src/hub/dataindex/indexer.py
+++ src/hub/dataindex/indexer.py
@@ -7,7 +7,8 @@ import biothings.hub.dataindex.indexer as indexer
 class GeneIndexer(indexer.Indexer):
 
     def get_index_creation_settings(self):
-        return {"codec" : "best_compression"}
+        return {"codec" : "best_compression",
+                "auto_expand_replicas": "0-all"}
 
     def get_mapping(self, enable_timestamp=True):
         mapping = super(GeneIndexer,self).get_mapping(enable_timestamp)
