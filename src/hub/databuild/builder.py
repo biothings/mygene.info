f6f9e614e55d3e1e39606e6fced721fba279e323
Sebastien Lelong <sebastien.lelong@gmail.com>
2018-02-05 14:13:29 -0800

    improve stats computation

--- src/hub/databuild/builder.py
+++ src/hub/databuild/builder.py
@@ -29,23 +29,52 @@ class MyGeneDataBuilder(builder.DataBuilder):
             return cleaner
 
     def post_merge(self, source_names, batch_size, job_manager):
-        self.logger.info("Indexing 'taxid'")
-        # background=true or it'll lock the whole database...
         tgt = mongo.get_target_db()[self.target_name]
+        # background=true or it'll lock the whole database...
+        self.logger.info("Indexing 'taxid'")
         tgt.create_index("taxid",background=True)
 
     def get_stats(self,sources,job_manager):
         self.stats = super(MyGeneDataBuilder,self).get_stats(sources,job_manager)
         # enrich with some specific mygene counts, specially regarding ensembl vs. entrez
-        entrez_col = mongo.get_src_db()[EntrezGeneUploader.name]
-        ensembl_col = mongo.get_src_db()[EnsemblGeneUploader.name]
         tgt = mongo.get_target_db()[self.target_name]
-        mapped = len(loadobj(("ensembl_gene__2entrezgene_list.pyobj", mongo.get_src_db()), mode='gridfs'))
         self.stats["total_genes"] = tgt.count()
-        self.stats["total_entrez_genes"] = entrez_col.count()
-        self.stats["total_ensembl_genes"] = ensembl_col.count()
+        # entrez genes are digits only (also, don't count entrez_gene collection,
+        # because tgt can be a subset, we have to work with the merged collection)
+        self.logger.debug("Counting 'total_entrez_genes'")
+        entrez_cnt = tgt.find({"_id":{"$regex":'''^\\d+$'''}},{"_id":1}).count()
+        self.stats["total_entrez_genes"] = entrez_cnt
+        # ensembl genes aount are taken from :
+        # 1. "ensembl" field, but it can a list => use aggregation. 
+        #    Note: "ensembl.0" means first element of the list, so it implicitely
+        #    select doc with a list. Finally, filtering with {$type:"array"} doesn't work because
+        #    mongo filters this on the most inner field (that's weird, but it is what is it...)
+        # 2. when document is root doc coming from ensembl_gene collection without a "ensembl" key ("orphan")
+        # Note: we can't create a sparce or conditional index to help querying "ensembl"
+        # because data is too long for an index key, and "hashed" mode doesn't work because list aren't supported
+        # Queries are gonna use colscan strategy...
+        self.logger.debug("Counting 'total_ensembl_genes'")
+        res = tgt.aggregate([
+            {"$match" : {"ensembl.0" : {"$exists" : True}}},
+            {"$project" : {"num_gene" : {"$size" : "$ensembl"}}},
+            {"$group" : {"_id" : None, "sum" : {"$sum": "$num_gene"}}}
+            ])
+        list_count = next(res)["sum"]
+        object_count = tgt.find({"ensembl" : {"$type" : "object"}},{"_id":1}).count()
+        orphan_count = tgt.find({"_id":{"$regex":'''\\w'''},"ensembl":{"$exists":0}},{"_id":1}).count()
+        total_ensembl_genes = list_count + object_count + orphan_count
+        self.stats["total_ensembl_genes"] = total_ensembl_genes
+        # this one can't be computed from merged collection, and is only valid when build
+        # involves all data (no filter, no subset)
+        self.logger.debug("Counting 'total_ensembl_genes_mapped_to_entrez'")
+        mapped = len(loadobj(("ensembl_gene__2entrezgene_list.pyobj", mongo.get_src_db()), mode='gridfs'))
         self.stats["total_ensembl_genes_mapped_to_entrez"] = mapped
-        self.stats["total_ensembl_only_genes"] = ensembl_col.count() - mapped
+        # ensembl gene contains letters (if it wasn't, it means it would only contain digits
+        # so it would be an entrez gene (\\D = non-digits, can't use \\w as a digit *is* a letter)
+        self.logger.debug("Counting 'total_ensembl_only_genes'")
+        ensembl_unmapped = entrez_cnt = tgt.find({"_id":{"$regex":'''\\D'''}},{"_id":1}).count()
+        self.stats["total_ensembl_only_genes"] = ensembl_unmapped
+        self.logger.debug("Counting 'total_species'")
         self.stats["total_species"] = len(tgt.distinct("taxid"))
 
         return self.stats
