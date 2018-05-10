5dfd21a9ae0e7913678e1ecf236c6ad7409fcd5c
Sebastien Lelong <slelong@scripps.edu>
2018-05-10 14:51:32 -0700

    switch to NCBI2Reactome_All_Levels.txt file

--- src/hub/dataload/sources/reactome/dump.py
+++ src/hub/dataload/sources/reactome/dump.py
@@ -15,6 +15,6 @@ class ReactomeDumper(LastModifiedHTTPDumper):
     SRC_NAME = "reactome"
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
     # URL is always the same, but headers change
-    SRC_URLS = ["https://reactome.org/download/current/NCBI2Reactome.txt"]
+    SRC_URLS = ["https://reactome.org/download/current/NCBI2Reactome_All_Levels.txt"]
     SCHEDULE = "0 6 * * *"
 
