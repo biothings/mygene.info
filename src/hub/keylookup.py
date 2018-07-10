98fa0a866ce06ebffc4332d102f8e2cd130bcb3e
Sebastien Lelong <slelong@scripps.edu>
2018-07-10 15:28:37 -0700

    define mygene keylookup graph + register it plugin assistant so plugin can declare _id conversion from manifest.json files

--- src/hub/keylookup.py
+++ src/hub/keylookup.py
@@ -4,6 +4,7 @@ from biothings.utils.keylookup import KeyLookup
 graph_mygene = nx.DiGraph()
 
 graph_mygene.add_node('entrez')
+graph_mygene.add_node('ensembl')
 graph_mygene.add_node('uniprot')
 
 graph_mygene.add_edge('uniprot', 'entrez',
@@ -11,14 +12,19 @@ graph_mygene.add_edge('uniprot', 'entrez',
             'lookup': 'uniprot.Swiss-Prot',
             'field': '_id'})
 
+graph_mygene.add_edge('uniprot', 'entrez',
+        object={'col': 'uniprot',
+            'lookup': 'uniprot.TrEMBL',
+            'field': '_id'})
+
 # TODO: conversions from ensembl to entrez should be added but mappings are currently
 # computed and stored in mongo as files, not collections, so they can't be queried
 
 class MyGeneKeyLookup(KeyLookup):
     collections = ['uniprot']
-    def __init__(self, input_type, skip_on_failure=False):
+    def __init__(self, input_types, skip_on_failure=False):
         super(MyGeneKeyLookup, self).__init__(graph_mygene,
-                self.collections, input_type,
-                output_types=["entrez"],
+                self.collections, input_types,
+                output_types=["entrez","ensembl"],
                 skip_on_failure=skip_on_failure)
 
