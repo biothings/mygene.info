644f2e3b1f8b8db830ad813033f6e11afc6f0e92
kevinxin90 <kevinxin@scripps.edu>
2018-07-25 15:51:07 -0700

    update pharos parser to fix 1 entrez gene id matching multiple pahros id issue

--- src/hub/dataload/sources/pharos/parser.py
+++ src/hub/dataload/sources/pharos/parser.py
@@ -1,15 +1,16 @@
-from biothings.utils.dataload import open_anyfile
+from biothings.utils.dataload import open_anyfile, unlist
+from collections import defaultdict
 
 
 def load_data(input_file):
 
     with open_anyfile(input_file) as in_f:
+        result = defaultdict(list)
         for line in in_f:
             pharos_id, _id = line.strip().split(',')
-            if _id != 'entrez_gene_id':
-                yield {
-                    '_id': int(_id),
-                    'pharos' : {"target_id" : int(pharos_id)},
-                }
-
-
+            if _id != 'entrez_gene_id' and _id != '0':
+                result[str(_id)].append(int(pharos_id))
+        for k, v in result.items():
+            json_doc = {'_id': str(k),
+                        'pharos': {"target_id": v}}
+            yield unlist(json_doc)
