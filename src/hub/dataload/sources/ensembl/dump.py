5ff099870d5ca3d8b07f1658dc2a65d3ef7aefc9
Sebastien Lelong <sebastien.lelong@gmail.com>
2018-07-25 17:39:21 -0700

    restore default schedule + refactored ensembl dumper

--- src/hub/dataload/sources/ensembl/dump.py
+++ src/hub/dataload/sources/ensembl/dump.py
@@ -55,6 +55,8 @@ class BioMart(HTTPDumper):
     # define which fields to get from biotmart
     DUMP_METHOD = {}
 
+    SCHEDULE = "0 6 * * *"
+
     # xml query template, must be defined in subclass
     TEMPLATE = None
 
