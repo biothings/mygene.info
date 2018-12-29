433da9eb9b5877000db736c5b2d87bbcf6d03b92
Jerry <xzhou@scripps.edu>
2018-12-28 19:43:43 -0800

    Applied refactoring to all ensembl dumpers

--- src/hub/dataload/sources/ensembl_fungi/dump.py
+++ src/hub/dataload/sources/ensembl_fungi/dump.py
@@ -13,55 +13,13 @@ class EnsemblFungiBioMart(GenericBioMart):
     # used to get latest release number & list of available species
     ENSEMBL_FTP_HOST = "ftp.ensemblgenomes.org"
     MART_URL = "http://fungi.ensembl.org/biomart/martservice"
-    TEMPLATE = XML_QUERY_TEMPLATE
-    DUMP_METHOD = {"gene_ensembl__gene__main.txt":"get_gene__main",
-                 "gene_ensembl__translation__main.txt":"get_translation__main",
-                 "gene_ensembl__xref_entrezgene__dm.txt":"get_xref_entrezgene",
-                 "gene_ensembl__prot_profile__dm.txt":"get_profile",
-                 "gene_ensembl__prot_interpro__dm.txt":"get_interpro",
-                 "gene_ensembl__prot_pfam__dm.txt":"get_pfam"}
 
-    SCHEDULE = "0 6 * * *"
+    RELEASE_FOLDER = '/pub/fungi'
+    RELEASE_PREFIX = '/pub/fungi/release-'
 
-    def get_latest_mart_version(self):
-        ftp = FTP(self.__class__.ENSEMBL_FTP_HOST)
-        ftp.login()
-        release_li = [x for x in ftp.nlst('/pub/fungi') if x.startswith('/pub/fungi/release-')]
-        return str(sorted([int(fn.split('-')[-1]) for fn in release_li])[-1])
-
-    def select_species(self):
-        import tempfile
-        outfile = tempfile.mktemp() + '.txt.gz'
-        try:
-            self.logger.info('Downloading "dataset_names.txt.gz"...')
-            out_f = open(outfile, 'wb')
-            ftp = FTP(self.__class__.ENSEMBL_FTP_HOST)
-            ftp.login()
-            species_file = '/pub/fungi/release-%s/mysql/fungi_mart_%s/dataset_names.txt.gz' % (self.release, self.release)
-            ftp.retrbinary("RETR " + species_file, out_f.write)
-            out_f.close()
-            self.logger.info('Done.')
-
-            #load saved file
-            self.logger.info('Parsing "dataset_names.txt.gz"...')
-            species_li = tab2list(outfile, (0, 4, 5), header=0)
-            species_li = [[x[0]] + [x[2]] + [x[1]] for x in species_li]
-            species_li = [x[:-1] + [is_int(x[-1]) and int(x[-1]) or None] for x in species_li]
-            self.logger.info('Done.')
-        finally:
-            os.remove(outfile)
-            pass
-
-        import pprint
-        self.logger.error(pprint.pformat(species_li))
-        return species_li
-
+    def get_species_file(self):
+        return '/pub/fungi/release-%s/mysql/fungi_mart_%s/dataset_names.txt.gz' % (self.release, self.release)
 
     def get_virtual_schema(self):
         return 'fungi_mart'
 
-    def _get_species_table_prefix(self, species):
-        return species
-        
-    def get_dataset_name(self, species):
-        return '%s_gene' % self._get_species_table_prefix(species[0])
