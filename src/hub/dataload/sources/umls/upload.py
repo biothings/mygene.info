9d4c2eea17cb480dd4420b56a14c0e804ca244ba
jiwen xin <kevinxin@scripps.edu>
2020-04-07 09:20:50 -0700

    update new umls mapping file

--- src/hub/dataload/sources/umls/upload.py
+++ src/hub/dataload/sources/umls/upload.py
@@ -8,8 +8,7 @@ class UMLSUploader(uploader.BaseSourceUploader):
     name = "umls"
 
     def load_data(self, data_folder):
-        datafile = os.path.join(data_folder, 'MRCONSO.RRF')
-        umls_docs = load_data(datafile)
+        umls_docs = load_data(data_folder)
         return umls_docs
 
     @classmethod
@@ -22,6 +21,11 @@ class UMLSUploader(uploader.BaseSourceUploader):
                         "normalizer" : "keyword_lowercase_normalizer",
                         'copy_to': ['all'],
                     },
+                    "protein_cui": {
+                        "type": "keyword",
+                        "normalizer" : "keyword_lowercase_normalizer",
+                        'copy_to': ['all'],
+                    }
                 }
             }
         }
