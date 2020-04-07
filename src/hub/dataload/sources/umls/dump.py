5d024409b89dbe52388b5efa63d77d715532396c
jiwen xin <kevinxin@scripps.edu>
2020-04-07 10:57:04 -0700

    update dump;

--- src/hub/dataload/sources/umls/dump.py
+++ src/hub/dataload/sources/umls/dump.py
@@ -17,17 +17,7 @@ class UMLSDumper(ManualDumper):
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
     VERSION = '2020-4-7'
 
-    def __init__(self, *args, **kwargs):
-        super(UMLSDumper,self).__init__(*args,**kwargs)
-        self.logger.info("""
-Manually download from: https://download.nlm.nih.gov/umls/kss/2019AB/umls-2019AB-mrconso.zip
-""")
-
     def create_todump_list(self, force=True):
         self.release = VERSION
         local = os.path.join(SRC_ROOT_FOLDER, VERSION)
         self.to_dump.append({"remote":VERSION, "local":local})
-
-    def post_dump(self, *args, **kwargs):
-        self.logger.info("Unzipping files in '%s'" % self.new_data_folder) 
-        unzipall(self.new_data_folder)
