8907b1e5708ddcbd2a51ddbed08a63e30fb804c2
Jerry <xzhou@scripps.edu>
2020-04-07 23:22:05 -0700

    Add test code catalog

---
+++ src/tests/__init__.py
@@ -0,0 +1,15 @@
+'''
+    MyGene Data & Feature Tester
+
+    Run:
+    > pytest tests
+
+    Cases:
+    - 0** Network check
+    - 1** API annotation endpoint
+    - 2** API query endpoint
+    - 3** Web endpoint
+    - 4** Cornercase handling
+    - 5** Common control keywords
+    - 6** Datasources
+'''
