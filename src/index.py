fc1d47f4dd1e026df56de64d6a3ec815579ec8a3
Jerry <xzhou@scripps.edu>
2020-04-17 15:14:59 -0700

    Revise application entry point

--- src/index.py
+++ src/index.py
@@ -9,22 +9,22 @@
 """
 import os.path
 
+from biothings.web.index_base import main
 from tornado.web import StaticFileHandler
 
 import config
-from biothings.web.index_base import main
 
+DOCS_HANDLERS = []
 if config.INCLUDE_DOCS:
     if not os.path.exists(config.DOCS_STATIC_PATH):
         raise IOError('Run "make html" to generate sphinx docs first.')
-    else:
-        config.APP_LIST += [
-            (r"/widget/(.*)", StaticFileHandler, {
-                'path': os.path.join(config.STATIC_PATH, 'widget')}),
-            (r"/?(.*)", StaticFileHandler, {
-                'path': config.DOCS_STATIC_PATH}),
-        ]
+    DOCS_HANDLERS += [
+        (r"/widget/(.*)", StaticFileHandler, {
+            'path': os.path.join(config.STATIC_PATH, 'widget')}),
+        (r"/?(.*)", StaticFileHandler, {
+            'path': config.DOCS_STATIC_PATH}),
+    ]
 
 
 if __name__ == '__main__':
-    main()
+    main(DOCS_HANDLERS)
