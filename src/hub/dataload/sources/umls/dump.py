23bcff2b68450d8f92a68ce79fb2142dc430e40c
Sebastien Lelong <sebastien.lelong@gmail.com>
2020-04-20 08:14:03 -0700

    fix manual dumper definition

--- src/hub/dataload/sources/umls/dump.py
+++ src/hub/dataload/sources/umls/dump.py
@@ -15,9 +15,3 @@ class UMLSDumper(ManualDumper):
 
     SRC_NAME = "umls"
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
-    VERSION = '2020-4-7'
-
-    def create_todump_list(self, force=True):
-        self.release = VERSION
-        local = os.path.join(SRC_ROOT_FOLDER, VERSION)
-        self.to_dump.append({"remote":VERSION, "local":local})
