e22b66de7ec50d0f528808ce0f0dbf6bd9c41a15
Jerry <xzhou@scripps.edu>
2021-07-15 12:05:20 -0700

    organize tests

--- src/tests/app_tests/config.py
+++ src/tests/app_tests/config.py
@@ -1,27 +1,13 @@
 
-"""
-    Mychem.info
-    https://mychem.info/
-    Chemical and Drug Annotation as a Service.
-"""
-import os as _os
 import importlib.util as _imp_util
+from os.path import dirname, join, pardir
+import sys
 
-CONFIG_FILE_NAME = "config_web.py"
 
 # find the path of the config file
-_cfg_path = _os.path.abspath(_os.path.join(_os.path.pardir, CONFIG_FILE_NAME))
-while True:
-    if _os.path.exists(_cfg_path):
-        break
-    _new_path = _os.path.abspath(_os.path.join(
-        _os.path.join(_os.path.dirname(_cfg_path), _os.path.pardir),
-        CONFIG_FILE_NAME)
-    )
-    if _new_path == _cfg_path:
-        raise Exception(f"no config file {CONFIG_FILE_NAME} found")
-    else:
-        _cfg_path = _new_path
+_cfg_path = join(dirname(__file__), pardir, pardir)
+sys.path.append(_cfg_path)
+_cfg_path = join(_cfg_path, "config_web.py")
 
 # load config file using path
 _spec = _imp_util.spec_from_file_location("parent_config", _cfg_path)
@@ -32,9 +18,6 @@ _spec.loader.exec_module(_config)
 for _k, _v in _config.__dict__.items():
     if not _k.startswith('_'):
         globals()[_k] = _v
-
-# cleanup
-del CONFIG_FILE_NAME
 
 # override default
 ES_HOST = 'localhost:9200'
