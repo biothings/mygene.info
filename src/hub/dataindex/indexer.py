1923c93e23499df094c2d408e80f3a56792b4ccb
Jerry <xzhou@scripps.edu>
2021-08-08 14:43:14 -0700

    simplify indexer customization

--- src/hub/dataindex/indexer.py
+++ src/hub/dataindex/indexer.py
@@ -5,16 +5,16 @@ class GeneIndexer(Indexer):
 
     def __init__(self, build_doc, indexer_env, target_name, index_name):
         super().__init__(build_doc, indexer_env, target_name, index_name)
-        self.index_settings["codec"] = "best_compression"  # mygene's specific
-        self.index_settings["number_of_replicas"] = 0
-        self.index_settings["number_of_shards"] = 3
-        self.index_settings["analysis"]["tokenizer"] = {
+
+        # add a tokenizer
+        self.es_index_settings["analysis"]["tokenizer"] = {
             "refseq_tokenizer": {
                 "delimiter": ".",
                 "type": "path_hierarchy"
             }
         }
-        self.index_settings["analysis"]["analyzer"]["refseq_analyzer"] = {
+        # add an analyzer
+        self.es_index_settings["analysis"]["analyzer"]["refseq_analyzer"] = {
             "filter": "lowercase",
             "tokenizer": "refseq_tokenizer",
             "type": "custom"
