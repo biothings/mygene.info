cae10a843ad9576de72da14771a14ba7bf97428e
Zhongchao Qian <zqian@scripps.edu>
2021-11-02 15:53:47 -0700

    fix indentations and add exons_mm10 mapping

--- src/hub/dataload/sources/ucsc/upload.py
+++ src/hub/dataload/sources/ucsc/upload.py
@@ -3,32 +3,33 @@ import biothings.hub.dataload.uploader as uploader
 
 
 class UCSCExonsUploader(uploader.MergerSourceUploader):
-
     name = "ucsc_exons"
     main_source = "ucsc"
 
-    def load_data(self,data_folder):
-    	genedoc_d = load_ucsc_exons(data_folder)
-    	return genedoc_d
+    def load_data(self, data_folder):
+        genedoc_d = load_ucsc_exons(data_folder)
+        return genedoc_d
 
     @classmethod
     def get_mapping(klass):
-	    mapping = {
-	        #do not index exons
-	        "exons":  {
-	            "type": "object",
-	            "enabled": False
-	        },
-	        #do not index exons_hg19
-	        "exons_hg19":  {
-	            "type": "object",
-	            "enabled": False
-	        },
-	        "exons_mm9":  {
-	            "type": "object",
-	            "enabled": False
-	        }
-	    }
-	    return mapping
-
-
+        mapping = {
+            # do not index exons
+            "exons": {
+                "type": "object",
+                "enabled": False
+            },
+            # do not index exons_hg19
+            "exons_hg19": {
+                "type": "object",
+                "enabled": False
+            },
+            "exons_mm9": {
+                "type": "object",
+                "enabled": False
+            },
+            "exons_mm10": {
+                "type": "object",
+                "enabled": False
+            },
+        }
+        return mapping
