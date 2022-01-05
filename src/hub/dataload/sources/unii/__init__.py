439c56bbe5cf20698bab7868221064abc6eba412
jal347 <linjason.03@gmail.com>
2022-01-05 23:13:24 +0000

    made uploader

--- src/hub/dataload/sources/unii/__init__.py
+++ src/hub/dataload/sources/unii/__init__.py
@@ -1,3 +1,3 @@
 from .dumper import MyGeneUNIIDumper
-
+from .uploader import UniiUploader
 # TODO: Implement uploader, mapping, and metadata
---
+++ src/hub/dataload/sources/unii/uploader.py
@@ -0,0 +1,41 @@
+import os
+from ast import literal_eval
+import biothings, config
+biothings.config_for_app(config)
+
+import biothings.hub.dataload.uploader
+
+class UniiUploader(biothings.hub.dataload.uploader.BaseSourceUploader):
+    name = "mygene_unii"
+    __metadata__ = {
+        "src_meta": {
+            "url": "https://open.fda.gov/apis/other/substance/",
+            "license_url": "https://open.fda.gov/license/",
+            "license": "Creative Commons CC0 1.0 Universal"
+        }
+    }
+
+    idconverter = None
+    storage_class = biothings.hub.dataload.storage.IgnoreDuplicatedStorage
+
+    def load_data(self, data_folder):
+        self.logger.info("Load data from directory: '%s'" % data_folder)
+        infile = os.path.join(data_folder,"gene_unii.ndjson")
+        assert os.path.exists(infile)
+        with open(infile, 'r') as f:
+            for line in f:
+                doc = literal_eval(line)
+                yield doc
+
+
+    @classmethod
+    def get_mapping(klass):
+        mapping = {
+            "unii": {
+                "type": "keyword",
+                "normalizer" : "keyword_lowercase_normalizer",
+            },
+        }
+        return mapping
+    
+
