bedb1edca6573aa5647f54745ff2ba15fac84b07
Chunlei Wu <newgene@users.noreply.github.com>
2022-07-20 10:14:08 -0700

    simplified HTML_OUT_TITLE value in config_web

--- src/config_web.py
+++ src/config_web.py
@@ -32,7 +32,7 @@ APP_LIST += [
 HTML_OUT_HEADER_IMG = "/static/favicon.ico"
 
 # for title line on format=html
-HTML_OUT_TITLE = """<p style="font-family:'Open Sans',sans-serif;font-weight:bold; font-size:16px;"><a href="http://mygene.info" target="_blank" style="text-decoration: none; color: black">MyGene.info - Gene Annotation as a Service</a></p>"""
+HTML_OUT_TITLE = """<a href="http://mygene.info" target="_blank">MyGene.info - Gene Annotation as a Service</a>"""
 METADATA_DOCS_URL = "http://docs.mygene.info/en/latest/doc/data.html"
 QUERY_DOCS_URL = "http://docs.mygene.info/en/latest/doc/query_service.html"
 ANNOTATION_DOCS_URL = "http://docs.mygene.info/en/latest/doc/annotation_service.html"
