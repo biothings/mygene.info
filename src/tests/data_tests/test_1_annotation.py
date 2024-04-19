9c1f54946046c5ca463cd90a56d2b1e4795c6483
ctrl-schaff <59380780+ctrl-schaff@users.noreply.github.com>
2024-04-19 09:26:52 -0700

    Add the CURIE ID support to the mygene configuration (#143)
    * Add the CURIE ID support to the mygene configuration
    
    * Update variable naming for improved contextual meaning
    
    * Add the actual biothings default regex pattern
    
    * Add additional comments related to the different patterns
    
    * Update the regex pattern to capture non-word, non-digit ...
    
    We need to be able to capture expressions like ensembl.gene
    and uniprot.Swiss-Prot which were not captured by the default
    pattern matching mechanism.
    
    <Per regex101>
    Named Capture Group scope (?P<scope>[\w\W]+)
    Match a single character present in the list below [\w\W]
    + matches the previous token between one and unlimited times, as many times as possible, giving back as needed (greedy)
    \w matches any word character (equivalent to [a-zA-Z0-9_])
    \W matches any non-word character (equivalent to [^a-zA-Z0-9_])
    
    * Add the GET and POST tests for the server CURIE ID support ...
    
    Currently the mygene.info host doesn't support the CURIE id queries so
    the tests will only pass locally until the host is setup to handle that
    parsing which is enabled in this PR. The tests can be unmarked after
    we've verified that the results we get match the expected values
    
    * Remove the header mapping. Let it generate at runtime
    
    * Remove the default regex pattern ...
    
    Want to avoid "publically" exposing this and accidently changing
    expected behavior
    
    * Add additional tests
    
    * Re-add the xfail decorator
    
    * Add POST batch failure case test
    
    * Add multimatch tests with CURIE ID support
    
    * Correct docstring comment about return documents
    
    ---------
    
    Co-authored-by: Johnathan Schaff <jschaff@scripps.edu>

--- src/tests/data_tests/test_1_annotation.py
+++ src/tests/data_tests/test_1_annotation.py
@@ -1,3 +1,6 @@
+import pytest
+import requests
+
 from biothings.tests.web import BiothingsDataTest
 
 
@@ -177,6 +180,150 @@ class TestAnnotationGET(BiothingsDataTest):
     def test_113(self):
         self.request("gene/", expect=400)
 
+    @pytest.mark.xfail(
+        reason="CURIE ID SUPPORT NOT CURRENTLY ENABLED ON MYGENE.INFO HOST",
+        run=True,
+        strict=True,
+    )
+    def test_114(self):
+        """
+        Tests the annotation endpoint support for the biolink CURIE ID.
+
+        If support is enabled then we should retrieve the exact same document
+        for all the provided queries
+
+        A mirror copy of the tests we have in the biothings_client
+        package (gene.py)
+        """
+        curie_id_testing_collection = [
+            ("1017", "entrezgene:1017", "NCBIgene:1017"),
+            (1017, "entrezgene:1017", "ncbigene:1017"),
+            ("1017", "entrezgene:1017", "NCBIGENE:1017"),
+            ("1018", "ensembl.gene:ENSG00000250506", "ENSEMBL:ENSG00000250506"),
+            (1018, "ensembl.gene:ENSG00000250506", "ensembl:ENSG00000250506"),
+            (
+                "ENSG00000250506",
+                "ensembl.gene:ENSG00000250506",
+                "ENSEMBL:ENSG00000250506",
+            ),
+            (
+                "ensg00000250506",
+                "ensembl.gene:ENSG00000250506",
+                "ENSEMBL:ENSG00000250506",
+            ),
+            ("5995", "uniprot.Swiss-Prot:P47804", "UniProtKB:P47804"),
+            (5995, "uniprot.Swiss-Prot:P47804", "UNIPROTKB:P47804"),
+            ("5995", "uniprot.Swiss-Prot:P47804", "uniprotkb:P47804"),
+        ]
+
+        results_aggregation = []
+        endpoint = "gene"
+        for id_query, biothings_query, biolink_query in curie_id_testing_collection:
+            id_query_result = self.request(f"{endpoint}/{id_query}", expect=200)
+            assert isinstance(id_query_result, requests.models.Response)
+            assert id_query_result.url == self.get_url(path=f"{endpoint}/{id_query}")
+
+            biothings_term_query_result = self.request(
+                f"{endpoint}/{biothings_query}", expect=200
+            )
+            assert isinstance(biothings_term_query_result, requests.models.Response)
+            assert biothings_term_query_result.url == self.get_url(
+                path=f"{endpoint}/{biothings_query}"
+            )
+
+            biolink_term_query_result = self.request(
+                f"{endpoint}/{biolink_query}", expect=200
+            )
+            assert isinstance(biolink_term_query_result, requests.models.Response)
+            assert biolink_term_query_result.url == self.get_url(
+                path=f"{endpoint}/{biolink_query}"
+            )
+
+            results_aggregation.append(
+                (
+                    id_query_result.json() == biothings_term_query_result.json(),
+                    id_query_result.json() == biolink_term_query_result.json(),
+                    biothings_term_query_result.json()
+                    == biolink_term_query_result.json(),
+                )
+            )
+
+        results_validation = []
+        failure_messages = []
+        for result, test_query in zip(results_aggregation, curie_id_testing_collection):
+            cumulative_result = all(result)
+            if not cumulative_result:
+                failure_messages.append(
+                    f"Query Failure: {test_query} | Results: {result}"
+                )
+            results_validation.append(cumulative_result)
+
+        assert all(results_validation), "\n".join(failure_messages)
+
+    def test_115(self):
+        """
+        Tests the annotation endpoint support for the biolink CURIE ID.
+
+        Invalid query evaluating we still return properly return a missing document result
+        """
+        invalid_query = "tree:293484"
+        expected_invalid_status_code = 404
+        endpoint = "gene"
+        invalid_query_result = self.request(
+            f"{endpoint}/{invalid_query}", expect=expected_invalid_status_code
+        )
+        assert isinstance(invalid_query_result, requests.models.Response)
+        assert invalid_query_result.url == self.get_url(
+            path=f"{endpoint}/{invalid_query}"
+        )
+
+        expected_json_response = {"code": 404, "success": False, "error": "Not Found."}
+        assert invalid_query_result.json() == expected_json_response
+
+    def test_116(self):
+        """
+        Tests the annotation endpoint support for the biolink CURIE ID.
+
+        Evaluates the multimatch capabilities:
+        [Results validated as of 03/04/2024]
+        - (homologene.id:74409) which should return 21 documents
+        - (interpro.id:IPR000719) which should return over 1000 documents yielding a 500 error
+        """
+        valid_multimatch_query = "homologene.id:74409"
+        expected_multimatch_status_code = 200
+        endpoint = "gene"
+        multimatch_query_result = self.request(
+            f"{endpoint}/{valid_multimatch_query}",
+            expect=expected_multimatch_status_code,
+        )
+        assert isinstance(multimatch_query_result, requests.models.Response)
+        assert multimatch_query_result.url == self.get_url(
+            path=f"{endpoint}/{valid_multimatch_query}"
+        )
+
+        assert len(multimatch_query_result.json()) == 21
+
+        invalid_multimatch_query = "interpro.id:IPR000719"
+        expected_multimatch_status_code = 500
+        endpoint = "gene"
+        invalid_multimatch_query_result = self.request(
+            f"{endpoint}/{invalid_multimatch_query}",
+            expect=expected_multimatch_status_code,
+        )
+        assert isinstance(invalid_multimatch_query_result, requests.models.Response)
+        assert invalid_multimatch_query_result.url == self.get_url(
+            path=f"{endpoint}/{invalid_multimatch_query}"
+        )
+
+        expected_massive_multimatch_result = {
+            "code": 500,
+            "success": False,
+            "error": "Too Many Matches.",
+        }
+        assert (
+            invalid_multimatch_query_result.json() == expected_massive_multimatch_result
+        )
+
 
 class TestAnnotationPOST(BiothingsDataTest):
     host = "mygene.info"
@@ -264,3 +411,117 @@ class TestAnnotationPOST(BiothingsDataTest):
         # get retired gene
         res = self.request("gene", method="POST", data={"ids": "791256"}).json()
         assert res[0]["_id"] == "50846"  # this is the corresponding _id field
+
+    @pytest.mark.xfail(
+        reason="CURIE ID SUPPORT NOT CURRENTLY ENABLED ON MYGENE.INFO HOST",
+        run=True,
+        strict=True,
+    )
+    def test_156(self):
+        """
+        Tests the annotations endpoint support for the biolink CURIE ID.
+
+        Batch query testing against the POST endpoint to verify that the CURIE ID can work with
+        multiple
+
+        If support is enabled then we should retrieve the exact same document for all the provided
+        queries
+
+        A mirror copy of the tests we have in the biothings_client
+        package (gene.py)
+        """
+        curie_id_testing_collection = [
+            ("1017", "entrezgene:1017", "NCBIgene:1017"),
+            (1017, "entrezgene:1017", "ncbigene:1017"),
+            ("1017", "entrezgene:1017", "NCBIGENE:1017"),
+            ("1018", "ensembl.gene:ENSG00000250506", "ENSEMBL:ENSG00000250506"),
+            (1018, "ensembl.gene:ENSG00000250506", "ensembl:ENSG00000250506"),
+            (
+                "ENSG00000250506",
+                "ensembl.gene:ENSG00000250506",
+                "ENSEMBL:ENSG00000250506",
+            ),
+            (
+                "ensg00000250506",
+                "ensembl.gene:ENSG00000250506",
+                "ENSEMBL:ENSG00000250506",
+            ),
+            ("5995", "uniprot.Swiss-Prot:P47804", "UniProtKB:P47804"),
+            (5995, "uniprot.Swiss-Prot:P47804", "UNIPROTKB:P47804"),
+            ("5995", "uniprot.Swiss-Prot:P47804", "uniprotkb:P47804"),
+        ]
+
+        results_aggregation = []
+        endpoint = "gene"
+        for id_query, biothings_query, biolink_query in curie_id_testing_collection:
+            base_result = self.request(f"{endpoint}/{id_query}", expect=200)
+
+            query_collection = (id_query, biothings_query, biolink_query)
+            delimiter = ","
+            data_mapping = {
+                "ids": delimiter.join([f'"{query}"' for query in query_collection])
+            }
+            query_results = self.request(
+                endpoint, method="POST", data=data_mapping
+            ).json()
+            assert len(query_results) == len(query_collection)
+
+            batch_id_query = query_results[0]
+            batch_biothings_query = query_results[1]
+            batch_biolink_query = query_results[2]
+
+            batch_id_query_return_value = batch_id_query.pop("query")
+            assert batch_id_query_return_value == str(id_query)
+
+            batch_biothings_query_return_value = batch_biothings_query.pop("query")
+            assert batch_biothings_query_return_value == str(biothings_query)
+
+            batch_biolink_query_return_value = batch_biolink_query.pop("query")
+            assert batch_biolink_query_return_value == str(biolink_query)
+
+            batch_result = (
+                base_result.json() == batch_id_query,
+                base_result.json() == batch_biothings_query,
+                base_result.json() == batch_biolink_query,
+            )
+            results_aggregation.append(batch_result)
+
+        results_validation = []
+        failure_messages = []
+        for result, test_query in zip(results_aggregation, curie_id_testing_collection):
+            cumulative_result = all(result)
+            if not cumulative_result:
+                failure_messages.append(
+                    f"Query Failure: {test_query} | Results: {result}"
+                )
+            results_validation.append(cumulative_result)
+
+        assert all(results_validation), "\n".join(failure_messages)
+        assert all(results_validation), "\n".join(failure_messages)
+
+    def test_157(self):
+        """
+        Tests the annotation endpoint support for the biolink CURIE ID.
+
+        Invalid query evaluating we still return properly return a missing document result
+        via the POST batch
+        """
+        invalid_queries = ["tree:293484", "branch:844"]
+        expected_invalid_status_code = 400
+        endpoint = "gene"
+
+        delimiter = ","
+        data_mapping = {
+            "ids": delimiter.join([f'"{query}"' for query in invalid_queries])
+        }
+
+        invalid_query_results = self.request(
+            endpoint, expect=expected_invalid_status_code, data=data_mapping
+        ).json()
+        expected_batch_json_response = {
+            "code": 400,
+            "success": False,
+            "error": "Bad Request",
+            "missing": "id",
+        }
+        assert invalid_query_results == expected_batch_json_response
