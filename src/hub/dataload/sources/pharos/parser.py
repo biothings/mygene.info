dd05f39e4e43fa6e6c132492c977b27279bb89a3
Jason Lin <35415519+jal347@users.noreply.github.com>
2024-07-19 15:36:08 -0700

    pharos temporary hotfix (#151)
    pharos bugfix

--- src/hub/dataload/sources/pharos/parser.py
+++ src/hub/dataload/sources/pharos/parser.py
@@ -1,16 +1,33 @@
-from biothings.utils.dataload import open_anyfile, unlist
 from collections import defaultdict
 
+from biothings.utils.dataload import open_anyfile, unlist
 
-def load_data(input_file):
+
+def parse_tdl(input_file):
+    with open_anyfile(input_file) as f:
+        result = {}
+        for line in f:
+            (
+                _,
+                _,
+                _id,
+                tdl,
+            ) = line.strip().split(",")
+            if _id and _id != "NCBI_id" and _id != "0":
+                result[str(_id)] = tdl
+        return result
+
+
+def load_data(input_file, tdl_file):
+    entrez_tdls = parse_tdl(tdl_file)
 
     with open_anyfile(input_file) as in_f:
         result = defaultdict(list)
         for line in in_f:
-            pharos_id, _id = line.strip().split(',')
-            if _id != 'entrez_gene_id' and _id != '0':
+            pharos_id, _id = line.strip().split(",")
+            if _id != "entrez_gene_id" and _id != "0":
                 result[str(_id)].append(int(pharos_id))
         for k, v in result.items():
-            json_doc = {'_id': str(k),
-                        'pharos': {"target_id": v}}
-            yield unlist(json_doc)
+            if tdl := entrez_tdls.get(k):
+                json_doc = {"_id": str(k), "pharos": {"target_id": v}, "tdl": tdl}
+                yield unlist(json_doc)
--- src/hub/dataload/sources/pharos/upload.py
+++ src/hub/dataload/sources/pharos/upload.py
@@ -1,14 +1,30 @@
+import os
+
 import biothings.hub.dataload.uploader as uploader
 
-class PharosUploader(uploader.DummySourceUploader):
+from .parser import load_data
+
+
+class PharosUploader(uploader.BaseSourceUploader):
     name = "pharos"
+
+    def load_data(self, data_folder):
+        pharos_path = os.path.join(data_folder, "pharos_target_mapping.csv")
+        pharos_tdl_path = os.path.join(data_folder, "tcrd_tdl.csv")
+        data = load_data(pharos_path, pharos_tdl_path)
+        for doc in data:
+            yield doc
 
     @classmethod
     def get_mapping(self):
         mapping = {
             "pharos": {
                 "properties": {
-                    "target_id":  {"type": "integer"}
+                    "target_id": {"type": "integer"},
+                    "tdl": {
+                        "type": "keyword",
+                        "normalizer": "keyword_lowercase_normalizer",
+                    },
                 }
             }
         }
