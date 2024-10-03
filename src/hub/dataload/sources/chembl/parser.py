c3df992812467e7adb9726b58afe38088fe29b24
jal347 <linjason.03@gmail.com>
2024-10-03 12:08:32 -0700

    added comments and testing without keylookup

--- src/hub/dataload/sources/chembl/parser.py
+++ src/hub/dataload/sources/chembl/parser.py
@@ -3,6 +3,7 @@ import re
 
 
 def parse_data(data):
+    # https://www.uniprot.org/help/accession_numbers
     UNIPROT_ACCESSION_PATTERN = re.compile(
         r"[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}"
     )
