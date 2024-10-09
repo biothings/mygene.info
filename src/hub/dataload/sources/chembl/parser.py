619bcf2ea13bcd7c6abb97ebd9d166d5e76bbba3
jal347 <linjason.03@gmail.com>
2024-10-08 18:36:15 -0700

    changed parser instead

--- src/hub/dataload/sources/chembl/parser.py
+++ src/hub/dataload/sources/chembl/parser.py
@@ -3,6 +3,8 @@ import re
 
 
 def parse_data(data):
+    chembl_dict = {}
+
     # https://www.uniprot.org/help/accession_numbers
     UNIPROT_ACCESSION_PATTERN = re.compile(
         r"[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}"
@@ -20,13 +22,23 @@ def parse_data(data):
                 uniprot_accessions.append(accession)
         if uniprot_accessions:
             for uniprot_accession in uniprot_accessions:
-                output = {
-                    "chembl": {
-                        "chembl_target": item["target_chembl_id"],
-                        "uniprot_accession": uniprot_accession,
-                    },
-                }
-                yield output
+                uniprot = uniprot_accession
+                chembl_target = item["target_chembl_id"]
+                if uniprot in chembl_dict:
+                    if isinstance(chembl_dict[uniprot]["chembl_target"], list):
+                        chembl_dict[uniprot]["chembl_target"].append(chembl_target)
+                    else:
+                        chembl_dict[uniprot]["chembl_target"] = [
+                            chembl_dict[uniprot]["chembl_target"],
+                            chembl_target,
+                        ]
+                else:
+                    chembl_dict[uniprot] = {
+                        "chembl_target": chembl_target,
+                        "uniprot": uniprot,
+                    }
+    for uniprot, chembl in chembl_dict.items():
+        yield {"chembl": chembl}
 
 
 def load_data(target_filepaths):
--- src/hub/dataload/sources/chembl/upload.py
+++ src/hub/dataload/sources/chembl/upload.py
@@ -9,52 +9,44 @@ from hub.datatransform.keylookup import MyGeneKeyLookup
 
 from .parser import load_data
 
+# class ChemblMergerStorage(storage.MergerStorage):
+#     @classmethod
+#     def merge_func(klass, doc1, doc2, **kwargs):
+#         doc2 = copy.copy(doc2)
+#         chembl_dict = {}
 
-class ChemblMergerStorage(storage.MergerStorage):
+#         for doc in [doc1, doc2]:
+#             chembl_list = doc.get("chembl", [])
+#             if not isinstance(chembl_list, list):
+#                 chembl_list = [chembl_list]
 
-    def merge_chembl(self, doc1, doc2):
-        self.logger.info("Merging %s and %s", doc1, doc2)
-        chembl_dict = {}
+#             for chembl in chembl_list:
+#                 uniprot = chembl.get("uniprot")
+#                 chembl_target = chembl.get("chembl_target")
 
-        for doc in [doc1, doc2]:
-            chembl_list = doc.get("chembl", [])
-            if not isinstance(chembl_list, list):
-                chembl_list = [chembl_list]
+#                 if uniprot in chembl_dict:
+#                     if isinstance(chembl_dict[uniprot]["chembl_target"], list):
+#                         chembl_dict[uniprot]["chembl_target"].append(chembl_target)
+#                     else:
+#                         chembl_dict[uniprot]["chembl_target"] = [
+#                             chembl_dict[uniprot]["chembl_target"],
+#                             chembl_target,
+#                         ]
+#                 else:
+#                     chembl_dict[uniprot] = {
+#                         "chembl_target": chembl_target,
+#                         "uniprot": uniprot,
+#                     }
 
-            for chembl in chembl_list:
-                uniprot = chembl.get("uniprot")
-                chembl_target = chembl.get("chembl_target")
-
-                if uniprot in chembl_dict:
-                    if isinstance(chembl_dict[uniprot]["chembl_target"], list):
-                        chembl_dict[uniprot]["chembl_target"].append(chembl_target)
-                    else:
-                        chembl_dict[uniprot]["chembl_target"] = [
-                            chembl_dict[uniprot]["chembl_target"],
-                            chembl_target,
-                        ]
-                else:
-                    chembl_dict[uniprot] = {
-                        "chembl_target": chembl_target,
-                        "uniprot": uniprot,
-                    }
-
-        merged_doc = {"_id": doc2["_id"]}
-        merged_doc["chembl"] = list(chembl_dict.values())
-        self.logger.info("Merged doc: %s", merged_doc)
-        return merged_doc
-
-    @classmethod
-    def merge_func(klass, doc1, doc2, **kwargs):
-        instance = klass()
-        doc = instance.merge_chembl(doc1, copy.copy(doc2))
-        return doc
+#         merged_doc = {"_id": doc2["_id"]}
+#         merged_doc["chembl"] = list(chembl_dict.values())
+#         return merged_doc
 
 
 class ChemblUploader(uploader.BaseSourceUploader):
     name = "chembl"
 
-    storage_class = ChemblMergerStorage
+    storage_class = storage.RootKeyMergerStorage
     TARGET_FILENAME_PATTERN = "target.*.json"
 
     keylookup = MyGeneKeyLookup(
