459feb8912e117e7f594fb8c35e517423b6fbd6f
Jason Lin <35415519+jal347@users.noreply.github.com>
2024-10-14 11:33:06 -0700

    Chembl source and orthology mapping fix (#157)

--- src/hub/dataload/sources/chembl/upload.py
+++ src/hub/dataload/sources/chembl/upload.py
@@ -1,15 +1,49 @@
+import copy
+import glob
+import os
+
+import biothings.hub.dataload.storage as storage
 import biothings.hub.dataload.uploader as uploader
 
+from hub.datatransform.keylookup import MyGeneKeyLookup
 
-class ChemblUploader(uploader.DummySourceUploader):
+from .parser import load_data
+
+
+class ChemblUploader(uploader.BaseSourceUploader):
     name = "chembl"
+
+    storage_class = storage.RootKeyMergerStorage
+    TARGET_FILENAME_PATTERN = "target.*.json"
+
+    keylookup = MyGeneKeyLookup(
+        input_types=[
+            ("swissprot", "chembl.uniprot"),
+            ("trembl", "chembl.uniprot"),
+        ],
+        skip_on_failure=True,
+    )
+
+    def load_data(self, data_folder):
+        target_filepaths = glob.iglob(
+            os.path.join(data_folder, self.TARGET_FILENAME_PATTERN)
+        )
+        return self.keylookup(load_data)(target_filepaths)
 
     @classmethod
     def get_mapping(klass):
         mapping = {
-            "chembl_target": {
-                "type": "keyword",
-                "normalizer": "keyword_lowercase_normalizer",
-            }
+            "chembl": {
+                "properties": {
+                    "chembl_target": {
+                        "type": "keyword",
+                        "normalizer": "keyword_lowercase_normalizer",
+                    },
+                    "uniprot_accession": {
+                        "type": "keyword",
+                        "normalizer": "keyword_lowercase_normalizer",
+                    },
+                }
+            },
         }
         return mapping
--- src/plugins/orthology_agr/mapping.py
+++ src/plugins/orthology_agr/mapping.py
@@ -1,35 +1,25 @@
 def get_customized_mapping(cls):
-    mapping={
-    "agr": {
-        "properties": {
-            "orthologs": {
-                "properties": {
-                    "geneid": {
-                        "normalizer": "keyword_lowercase_normalizer",
-                        "type": "keyword"
-                    },
-                    "symbol": {
-                        "normalizer": "keyword_lowercase_normalizer",
-                        "type": "keyword"
-                    },
-                    "taxid": {
-                        "type": "integer"
-                    },
-                    "algorithmsmatch": {
-                        "type": "integer"
-                    },
-                    "outofalgorithms": {
-                        "type": "integer"
-                    },
-                    "isbestscore": {
-                        "type": "boolean"
-                    },
-                    "isbestrevscore": {
-                        "type": "boolean"
+    mapping = {
+        "agr": {
+            "properties": {
+                "orthologs": {
+                    "properties": {
+                        "geneid": {
+                            "normalizer": "keyword_lowercase_normalizer",
+                            "type": "keyword",
+                        },
+                        "symbol": {
+                            "normalizer": "keyword_lowercase_normalizer",
+                            "type": "keyword",
+                        },
+                        "taxid": {"type": "integer"},
+                        "algorithmsmatch": {"type": "integer"},
+                        "outofalgorithms": {"type": "integer"},
+                        "isbestscore": {"type": "boolean"},
+                        "isbestrevscore": {"type": "boolean"},
                     }
                 }
             }
         }
     }
-}
-    return mapping;
+    return mapping
