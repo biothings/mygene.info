67d91fd06bf1a1ee0ff00e51f680ca1f6d6ba254
Guoxuan Xu <guoxuan.xu8@gmail.com>
2025-02-11 13:09:41 -0800

    Update mapping.py function of the cell_marker plugin

--- src/plugins/cell_marker/mapping.py
+++ src/plugins/cell_marker/mapping.py
@@ -2,31 +2,16 @@ def get_customized_mapping(cls):
     mapping = {
         "cellmarker": {
             "properties": {
-                "cellontology": {
-                    "normalizer": "keyword_lowercase_normalizer",
-                    "type": "keyword",
-                },
-                "pmid": {
-                    "normalizer": "keyword_lowercase_normalizer",
-                    "type": "keyword",
-                },
-                "species": {
-                    "normalizer": "keyword_lowercase_normalizer",
-                    "type": "keyword",
-                },
-                "uberon": {
-                    "normalizer": "keyword_lowercase_normalizer",
-                    "type": "keyword",
-                },
-                "company": {
-                    "normalizer": "keyword_lowercase_normalizer",
-                    "type": "keyword",
-                },
-                "cancertype": {"type": "text"},
-                "marker_resource": {"type": "text"},
-                "tissue": {"type": "text"},
-                "cellname": {"type": "text"},
-                "celltype": {"type": "text"},
+                "cancertype": {"normalizer": "keyword_lowercase_normalizer", "type": "keyword"},
+                "cellname": {"normalizer": "keyword_lowercase_normalizer", "type": "keyword"},
+                "cellontology": {"normalizer": "keyword_lowercase_normalizer", "type": "keyword"},
+                "celltype": {"normalizer": "keyword_lowercase_normalizer", "type": "keyword"},
+                "marker_resource": {"normalizer": "keyword_lowercase_normalizer", "type": "keyword"},
+                "pmid": {"normalizer": "keyword_lowercase_normalizer", "type": "keyword"},
+                "species": {"normalizer": "keyword_lowercase_normalizer", "type": "keyword"},
+                "tissue": {"normalizer": "keyword_lowercase_normalizer", "type": "keyword"},
+                "uberon": {"normalizer": "keyword_lowercase_normalizer", "type": "keyword"},
+                "company": {"normalizer": "keyword_lowercase_normalizer", "type": "keyword"},
             }
         }
     }
